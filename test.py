ea505074c2e4d5544bb3fe0d7140a12bc17da6eb
ppongnp <ppgxnp@gmail.com>
2020-12-13 23:37:43 +0700

    edit param of ADF test function

--- test.py
+++ test.py
@@ -31,7 +31,7 @@ from sklearn.metrics import median_absolute_error, mean_squared_error, mean_squa
 from itertools import product                    # some useful functions
 from tqdm import tqdm_notebook
 
-def ADF_Stationarity_Test(timeseries):
+def ADF_Stationarity_Test(timeseries,showResult=False):
     #Dickey-Fuller test:
     adfTest = adfuller(timeseries,autolag='AIC')
     pValue = adfTest[1]
@@ -40,8 +40,9 @@ def ADF_Stationarity_Test(timeseries):
     #Add Critical Values
     for key,value in adfTest[4].items():
         dfResults['Critical Value (%s)'%key] = value
-    #print('Augmented Dickey-Fuller Test Results:')
-    #print(dfResults)
+    if showResult:
+        print('Augmented Dickey-Fuller Test Results:')
+        print(dfResults)
     return dfResults
 
 def get_stationarity(dataset,showResult=False):
@@ -78,17 +79,17 @@ def get_NAV_dataframe(path):
     return nav
 
 # this is not Stationary ---> mean,var,covar is not constrant over period
-nav = get_NAV_dataframe('time-series-forecast/dataset/NAV-SI-1AM-TG.csv')
+nav = get_NAV_dataframe('time-series-forecast/dataset/NAV-SI-TMBEGRMF.csv')
 stationary_nav = get_stationarity(nav)
 x = stationary_nav.values
 
-train_num = round(len(x) * 0.8)
-test_num = round(len(x) * 0.2)
+train_num = round(len(x) * 0.7)
+test_num = round(len(x) * 0.3)
 
 train_set = x[0:train_num]
 test_set = x[train_num-1:]
 
-"""
+
 history = [x for x in train_set]
 prediction = list()
 for t in range(len(test_set)):
@@ -99,7 +100,7 @@ for t in range(len(test_set)):
     prediction.append(yhat)
     obs = test_set[t]
     history.append(obs)
-    print('predicted=%f, expected=%f' % (yhat, obs))
+    #print('predicted=%f, expected=%f' % (yhat, obs))
 
 error = mean_squared_error(test_set, prediction)
 print('Test MSE: %.5f' % error)
@@ -107,13 +108,11 @@ print('Test MSE: %.5f' % error)
 forecast_errors = [test_set[i]-prediction[i] for i in range(len(test_set))]
 bias = sum(forecast_errors) * 1.0/len(test_set)
 print('Bias: %f' % bias)
-"""
-# plot
-"""
+
 plt.plot(test_set)
 plt.plot(prediction,color='red')
 plt.show()
-"""
+
 """
 tototo = []
 for i in range(len(prediction)-1):
@@ -126,10 +125,9 @@ for i in range(len(prediction)-1):
 
 df2 = pd.DataFrame (tototo,columns=['value'])
 df2.plot()
-get_stationarity(df2)
+ADF_Stationarity_Test(df2)
 plt.show()
 """
-
 
 """
 model = ARIMA(train_set,order=(1,1,1))
